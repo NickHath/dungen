566520211b281c1ba65ef0d7a6aa2156dd0e7b90
Nick Hathaway <nicholashathaway@gmail.com>
2022-12-06 17:10:00 -0500

    add slatejs

---
+++ pages/editor/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+import { BaseEditor, Descendant } from 'slate'
+import { ReactEditor } from 'slate-react'
+
+type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module 'slate' {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initialValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: 'Test.' }],
+  },
+]
+
+export default function Editor() {
+  const [editor] = useState(() => withReact(createEditor()))
+  
+  return (
+  <Slate editor={editor} value={initialValue}>
+    <Editable />
+  </Slate>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -9,9 +9,14 @@ export default function Home() {
         <title>Dungen</title>
         <meta name="description" content="AI dungeons and dragons generator" />
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
       </Head>
 
       <main className={styles.main}>
+        Landing Page
+        <Link href="/editor">
+          Editor
+        </Link>
       </main>
 
       <footer className={styles.footer}>
