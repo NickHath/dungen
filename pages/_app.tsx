b9634e8f4a1885bf508ae14ace6fc2a876b7c0bc
Nick Hathaway <nicholashathaway@gmail.com>
2022-12-07 21:58:43 -0500

    flesh out landing page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import NavBar from '../src/components/nav-bar'
 import { Poppins } from '@next/font/google'
 import { useState } from 'react'
 import PromptContext from '../src/prompt-context'
+import Head from 'next/head'
 
 const poppins = Poppins({
   weight: ['200', '400', '500', '600'],
@@ -16,6 +17,12 @@ export default function App({ Component, pageProps }: AppProps) {
   const [prompt, setPrompt] = useState('')
   return (
     <main className={poppins.className}>
+      <Head>
+        <title>Dungen</title>
+        <meta name='description' content='AI dungeons and dragons generator' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta charSet='utf-8' />
+      </Head>
       <NavBar />
       <PromptContext.Provider value={{ prompt, setPrompt }}>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,22 @@
-import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Dungen</title>
-        <meta name='description' content='AI dungeons and dragons generator' />
-        <link rel='icon' href='/favicon.ico' />
-        <meta charSet='utf-8' />
-      </Head>
-
       <main className={styles.main}>
-        {/* Informative Landing Page to lay out process and explain how to come up with setting, then how it will generate session ideas for you */}
         <div className={styles.section}>
           <h1>AI-Powered D&D session ideas</h1>
           <p>Using <a href='' target='_external'>Open AI's GPT-3</a>, Dungen generates Dungeons and Dragons session ideas using a setting description. Simply use a template or type in your D&D setting background, then you can generate a list of session ideas informed by that setitng.</p>
         </div>
         <div className={styles.section}>
           <h1>How it works</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Dungen uses the AI model GPT-3, which is trained on a vast corpus of language data. Supported by OpenAI, it is able to generate text following a prompt. In other words, it completes a text with its best guesses for the next word in order. Dungen uses GPT-3 by giving it a D&D setting that you provide, adding additional details to the prompt, then sending it to OpenAI to obtain text generations of session ideas.</p>
         </div>
         <Link href='/editor'>
             <button>Generate Session Ideas</button>
         </Link>
       </main>
-
       <footer className={styles.footer}>
       </footer>
     </div>
