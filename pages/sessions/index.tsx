5af62456c41b0dca24da82c81fe3bb960ad409fd
Nick Hathaway <nicholashathaway@gmail.com>
2023-07-06 19:27:56 -0400

    add copy functionality and toast notification on copy

--- pages/sessions/index.tsx
+++ pages/sessions/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useContext, useState } from 'react'
 import PromptContext from '../../src/prompt-context'
 import { BarLoader } from 'react-spinners'
+import toast, { Toaster } from 'react-hot-toast'
 import styles from '../../styles/Sessions.module.css'
 
 export default function Sesions() {
@@ -14,15 +15,44 @@ export default function Sesions() {
       .catch(err => console.error(err))
   }, [])
 
+  const handleCopy = async (session: string) => {
+    try {
+      await navigator.clipboard.writeText(session)
+      toast('Session idea copied')
+    } catch (err) {
+      toast('Session failed to copy',
+        {
+          style: {
+            background: 'hsl(0, 100%, 50%)'
+          }
+        }
+      )
+    }
+  }
+
   const sessionsJSX = sessions.map((session, index) => (
     <div className={styles.drawer} key={index}>
       <h1>{`Session #${index + 1}`}</h1>
       <p>{session}...</p>
+      <button onClick={() => handleCopy(session)}>Copy Session</button>
     </div>
   ))
 
   return (
     <div className={styles.sessions}>
+      <Toaster
+        position='bottom-right'
+        toastOptions={{
+          style: {
+            background: 'hsl(30, 56%, 29%)',
+            color: '#FFF',
+            fontWeight: 400
+          },
+          success: {
+            duration: 3000
+          }
+        }}
+      />
       { sessionsJSX.length ? 
           sessionsJSX : 
           <BarLoader
